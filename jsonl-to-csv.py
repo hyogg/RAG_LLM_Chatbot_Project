07cfcdb61b1aef5e18532b1da9c00f8e846fc48f
Hyojeong <itshyoj@gmail.com>
2023-11-25 06:26:45 +0900

    Fix: 바로 실행되도록 버그 해결
    1. encoding 추가
    2. jsonl read문 들여쓰기 해결
    3. if __name__ == "__main__" 추가하여 직접 호출할 때만 실행되도록 함
    4. row.get 구문 없애고 (key값 인식을 잘 못함) 바로 row[header]로 넣어줌

--- jsonl-to-csv.py
+++ jsonl-to-csv.py
@@ -1,9 +1,5 @@
-import glob
 import json
 import csv
-import time
-
-start = time.time()
 from flatten_json import flatten
 
 def convert_to_csv(input_file_path:str = './files/data_v3.jsonl', output_file_path:str = './files/passage_dataset.csv', headers:list[str] = ['title', 'link', 'era', 'info', 'description']) :
@@ -13,18 +9,20 @@ def convert_to_csv(input_file_path:str = './files/data_v3.jsonl', output_file_pa
         output_file_path : Path of CSV file (output)
         headers : In the order -> [title, link, era, info(size, number, category.. etc), description(context-paragraph)]
         '''
-        with open (output_file_path, 'w', newline='') as c :
-            csvwriter = csv.writer(c, delimiter='|')
+        with open (output_file_path, 'w', newline='', encoding='utf-8-sig') as c :
+            csvwriter = csv.writer(c)
             csvwriter.writerow(headers)
 
-        with open(input_file_path, 'r') as f :
-            for line in f :
-                print('Processing line:', line)
-                data = json.loads(line)
-                row = flatten(data)
-                csvwriter.writerow([row.get(header, '') for header in headers])
+            with open(input_file_path, 'r', encoding='utf-8') as f :
+                for line in f :
+                    print('Processing line:', line)
+                    data = json.loads(line)
+                    row = flatten(data)
+                    csvwriter.writerow([row[header] for header in headers])
         print('Conversion completed successfully.')
         input('Press Enter to exit...')
     except Exception as e :
         print(f'An error occurred : {e}')
 
+if __name__ == '__main__' :
+    convert_to_csv()
