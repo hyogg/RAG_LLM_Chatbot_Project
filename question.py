8adeefa086a012026cf1549dabe471c8dd5f9a88
Hyojeong <itshyoj@gmail.com>
2023-11-27 03:06:19 +0900

    Refactor: Split Class
    Description of this class feature
    1. Load Document
    2. Split Document over 2000 tokens (by tiktoken)
    3. Merge split documents and Save as JSONL file
    
    Execute Function : process()

--- question.py
+++ question.py
@@ -1,121 +1,83 @@
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from tqdm.auto import tqdm
-import re
-import json
-import asyncio
-
-from dotenv import load_dotenv
-load_dotenv()
+from tqdm import tqdm
 import pandas as pd
-import openai
 import tiktoken
+import json
 
-def remove_number_dot(text):
-    pattern = r"^\d+\.\s+"
-    cleaned_text = re.sub(pattern, "", text)
-    return cleaned_text.strip()
+class generate_question :   
+    def __init__(self, jsonl_input_path: str = "./files/museum_passage.jsonl", jsonl_output_path: str = "./files/passage_split.jsonl") :
+        '''
+        jsonl_input_path: 크롤링 데이터 원본을 담고 있는 JSONL 파일의 경로
+        jsonl_output_path : 파일을 저장할 경로  
+        '''
+        self.jsonl_input_path = jsonl_input_path
+        self.jsonl_output_path = jsonl_output_path
 
-def tiktoken_len(text):
-    tokenizer = tiktoken.get_encoding('cl100k_base') # gpt 모델에 인코딩(토크나이징)할 때 쓰이는 임베딩 모델
-    tokens = tokenizer.encode(text)
-    # print(tokens)
-    return len(tokens)
+    def load_data(self):
+        '''
+        JsonL 파일을 로드해서 리스트로 리턴하는 함수
+        '''
+        data_list = []
+        with open(self.jsonl_input_path, 'r') as file:
+            for line in file:
+                json_data = json.loads(line)
+                data_list.append(json_data)
+        return data_list
 
-def tiktoken_split(data):
-    # tokenizer = tiktoken.get_encoding('cl100k_base') # gpt 모델에 tokenizing
-    # tokens = tokenizer.encode(data)
-    splitter = RecursiveCharacterTextSplitter(
-        chunk_size=2000, chunk_overlap=50, length_function=tiktoken_len
-    )
-    df = data
-    new_rows = []
-    for index, row in df.iterrows():
-        content = row['content']
-        split_data = splitter.create_documents([content])
-        # print(split_data)
-        for split_content in split_data : # list의 list이므로
-            new_row = row.copy()
-            new_row['content'] = split_content
-            new_rows.append(new_row)
-    new_df = pd.DataFrame(new_rows)
-    new_df.to_csv('split_25p.csv', encoding="utf-8-sig")
-    new_df.to_json('split_25p.jsonl', orient='records',lines=True,force_ascii=False)
-    return new_df
+    def tiktoken_len(self, text, encoding_name="cl100k_base"):
+        '''
+        tiktoken library를 활용해 토큰 길이를 계산하는 함수
+        1. text : 길이 측정할 텍스트
+        2. encoding_name : 토큰으로 변환할 방식 지정
+            - cl100k_base : gpt-3.5-turbo, gpt-4, text-embedding-ada-002 모델 사용시
+            - p50k_base : text-davinci-002, text-davinci-003 모델 사용시
+            - r50k_base (or gpt2) : GPT-3 models like "davinci"
+        '''
+        tokenizer = tiktoken.get_encoding('cl100k_base')
+        # tokenizer = tiktoken.encoding_for_model("gpt-3.5-turbo") # 위 코드로 안될 시 모델 이름 넣어서 이걸로 해보기
+        tokens = tokenizer.encode(text)
+        return len(tokens)
 
-def generate_question(
-    title,
-    country_and_era,
-    size_and_category,
-    content,
-    model='gpt-3.5-turbo',
-    timeout=50,
-    max_retries=3) :
-    load_dotenv()
-    response = openai.ChatCompletion.create(
-        model = model,
-        messages = [
-            {
-                'role' : 'system',
-                'content' : '너는 박물관 도슨트야. 제시된 작품설명 내에서 나올 수 있는 질문하나를 뽑은 뒤 그 설명을 기반으로만 답변을 생성해서 "질문: {q}, 답변: {a}" 형식으로 만들어줘.'
-            },
-            {
-                'role' : 'user',
-                'content' : f'작품설명 [\n 제목: {title} \n 국적/시대 : {country_and_era}, 크기/구분:{size_and_category}, \n 해설:{content}]'
-            }
-        ]
-    )
-    retries = 0
-    while retries <= max_retries :
-        if response.choices :
+    def tiktoken_split(self, data):
+        '''
+        원본 크롤링 파일 (.jsonl) 문장에서
+        설명이 2000토큰이 넘어갈 경우 Split함
+        data : 크롤링 파일
+        jsonl_output_path : 파일을 저장할 경로  
+        '''
+        splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(chunk_size=2000, chunk_overlap=100)
+        new_lines = []
+
+        for line in data:
+            # description 부분만 받아오기 -> 없으면 KeyError 발생
             try :
-                questions = response.choices[0].message['content'].strip()
-                return questions
-            except Exception as e:
-                print(f"{e} question 생성 실패 오류 발생, 재시도 중 ...")
-                retries += 1
-                if retries > max_retries:
-                    print(f"재시도 횟수 초과. 다음 요청으로 넘어갑니다. {e}")
-                    return []
-                print(f"{retries}/{max_retries}회 시도중.. 에러 : {e}")
-        else :
-            retries += 1
-            if retries > max_retries:
-                print(f"TimeOut 재시도 횟수 초과. 다음 요청으로 넘어갑니다. {e}")
-                return []
-            print(f"TimeOut 재시도중.. {retries}/{max_retries}회 에러 : {e}")
+                text = line['description']
+            except KeyError:
+                raise KeyError("작품 설명의 key값을 'description'으로 설정하세요.")
+            token_len = tiktoken_len(text)
 
-async def main(data):
-    qa_pairs = []
-    last_processed = 0
-    try :
-        data = tiktoken_split(data)
-    except Exception as tiktoken_e :
-        print(f'토큰 Split 에러: {tiktoken_e}')
-    try:
-        for index, row in tqdm(data.iterrows()):
-            print('생성중... 현재상태 ->',row['title'])
-            questions = generate_question(
-                row["title"], row["country_and_era"], row['size_and_category'], row["content"]
-            )
-            qa_pairs.append({'question':remove_number_dot(questions), 'title':row["title"], 'index':index, 'content':row['content']})
-            last_processed = index
+            # 토큰 길이가 2000개 넘을 경우에만 문서 Split
+            if token_len > 2000 :
+                split_data = splitter.split_text(text)
+                for single_split_data in split_data :
+                    new_line = line.copy()
+                    new_line['description'] = single_split_data
+                    new_lines.append(new_line)
+                    # print(f'split... -> {single_split_data}')
+            else :
+                new_lines.append(text)
+        
+        # new_lines를 jsonl 파일로 저장
+        with open(self.json_output_path, 'w', encoding='utf-8') as jsonl_file :
+            for dict in new_lines:
+                line = json.dump(dict)
+                jsonl_file.write(line + '\n')
+        return new_lines
 
-            # 실험용
-            # if index >= 2 : 
-            #     break
-    except Exception as e:
-        print(f"에러 발생 (last_processed index = {last_processed}): {e}")
-    return qa_pairs, last_processed
-
-
-data = pd.read_csv("25p.csv")
-
-qa_pairs, last_processed = asyncio.run(main(data))
-qa_df = pd.DataFrame(qa_pairs, columns=["question", 'title', 'index', 'content'])
-qa_df.to_csv("qa_25p_to_31p.csv", index=False, encoding="utf-8-sig")
-qa_df.to_json(
-    "qa_25p_to_31p_records.jsonl",
-    orient="records",
-    lines=True,
-    force_ascii=False,
-)
+    # 실행을 위한 함수
+    def process(self):
+        try :
+            data = self.load_data()
+            new_data = self.tiktoken_split(data)
+        except Exception as e :
+            print('Error 발생.. -> {e}')
